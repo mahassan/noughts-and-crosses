4df2dbba125ae7400e97e7fc3e19e017a12c42c3
Ammar Hassan <ammar@ammarhassan.co.uk>
2021-08-23 02:38:24 +0100

    trying to win 2 grid

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ const iterableBoxes = [...boxes];
 // 1 . user can click anywhere on board
 // 2. when first player click he/she will have X and box will be disabled
 let count = 0;
+let board = [];
 for (let i = 0; i < iterableAnchor.length; i++) {
   iterableAnchor[i].addEventListener("click", (e) => {
     e.preventDefault();
@@ -14,15 +15,27 @@ for (let i = 0; i < iterableAnchor.length; i++) {
     if (count <= 9) {
       if (count % 2 == 0) {
         e.target.innerHTML = "o";
+        board.push("o");
       } else {
         e.target.innerHTML = "X";
+        board.push("X");
       }
-    } else {
-      boxes.innerHTML = "Ammar wins";
     }
+    if (count == 2) {
+      if (board[0] == "X" && board[2] == "X") {
+        winnerDiv.innerText = "X wins";
+      }
+    }
+    console.log(board);
   });
 }
 // 3. when second player click he/she will have O and box will be disabled
+// 3.1. if the count is 2 check for both element innerText
+
+// 3.2 if both is X then X wins otherwise it looses
+//thought - maybe we need array and push each element into that array
+//if that array as x next to each other, they win
+
 // 4. if X or 0 are in digonal, vertical, horizantal they win
 // 5. Display who wins
 
