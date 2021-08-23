2a7cdd4f28af989b76cb3473a775ca08425c4538
Ammar Hassan <ammar@ammarhassan.co.uk>
2021-08-23 02:02:32 +0100

    fixed the fixed pattern now its random

--- js/app.js
+++ js/app.js
@@ -1,20 +1,30 @@
 const anchors = document.getElementsByTagName("a");
 const boxes = document.getElementsByTagName("div");
+const winnerDiv = document.getElementById("winner");
 const iterableAnchor = [...anchors];
 const iterableBoxes = [...boxes];
 
+// 1 . user can click anywhere on board
+// 2. when first player click he/she will have X and box will be disabled
+let count = 0;
 for (let i = 0; i < iterableAnchor.length; i++) {
   iterableAnchor[i].addEventListener("click", (e) => {
     e.preventDefault();
-    e.target.parentElement.href = "#";
-    if (i % 2 == 0) {
-      e.target.innerText = "X";
-      console.dir(e);
+    count++;
+    if (count <= 9) {
+      if (count % 2 == 0) {
+        e.target.innerHTML = "o";
+      } else {
+        e.target.innerHTML = "X";
+      }
     } else {
-      e.target.innerText = "0";
+      boxes.innerHTML = "Ammar wins";
     }
   });
 }
+// 3. when second player click he/she will have O and box will be disabled
+// 4. if X or 0 are in digonal, vertical, horizantal they win
+// 5. Display who wins
 
 // function clickedBox() {
 //   for (let i = 0; i < iterableBoxes.length; i++) {
